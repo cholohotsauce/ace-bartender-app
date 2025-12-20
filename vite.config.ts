a2776d1d42bb9981e14a41f94b95887c958fa805
Arcadio Vega <100605454+cholohotsauce@users.noreply.github.com>
2025-12-20 15:37:05 -0500

    Fix PWA paths + manifest scope for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-    base: '/ace-bartender-app/',
-})
+export default defineConfig(({ command }) => ({
+    base: command === 'build' ? '/ace-bartender-app/' : '/',
+}))
